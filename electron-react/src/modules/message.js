c2db6632b5358417454c4e667dfe902e907e2713
Will Bendick <wbendick@gmail.com>
2020-07-15 16:24:04 -0700

    adds create rl user wallet

--- electron-react/src/modules/message.js
+++ electron-react/src/modules/message.js
@@ -237,3 +237,13 @@ export const create_rl_admin = (interval, limit, pubkey, amount) => {
   };
   return action;
 };
+
+export const create_rl_admin = () => {
+  var action = walletMessage();
+  action.message.command = "create_new_wallet";
+  action.message.data = {
+    wallet_type: "rl_wallet",
+    rl_type: "user"
+  };
+  return action;
+};
